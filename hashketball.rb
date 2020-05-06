f8eba76b267b9314c5f411b1066d00b01d818f90
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-06 00:08:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,30 @@
 # Write your code here!
+require "pry"
+
+def game_hash()
+{
+  :home => {
+    :team_name => {},
+    :colors => {}
+    :players => {
+      :player_name => {"Alan Anderson"}, 
+      :number => 0, 
+      :shoe => 16, 
+      :points => 22, 
+      :rebounds => 12, 
+      :assists => 12, 
+      :steals => 3, 
+      :blocks => 1, 
+      :slam_dunks => 1 
+  },
+  
+  :away => {
+    :team_name => {},
+    :colors => {}
+    :players => {
+      :player_name => [jeff_adrien, bismack_biyombo, desagna_diop, ben_gordon, kemba_walker]
+    }
+}
+    
+  }
+end
