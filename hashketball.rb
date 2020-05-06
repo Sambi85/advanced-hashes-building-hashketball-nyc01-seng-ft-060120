5433a2e2d72695f2887b49259d54856a47b8eea2
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-06 00:48:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,7 @@ hash = {
     :team_name => {},
     :colors => {},
     :players => {
-      :player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1 
+      :player_name => ["Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1] 
       
     }
   },
