2e1b1043d525b45cd4aa26dfa83f668610666186
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-06 01:07:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ hash = {
   :home => {
     :team_name => {},
     :colors => {},
-    :players => { 
+    :players => [ 
       :player_name => "Alan Anderson",
       :number => 0,
       :shoe => 16,
@@ -18,8 +18,27 @@ hash = {
       :steals => 3,
       :blocks => 1,
       :slam_dunks => 1 
-    }
+    ]
   },
+  
+    home_players = [alan_anderson, reggie_evans, brook_lopez, mason_plumlee, jason_terry]
+      names = game_hash[:home][:players].map {|player| player[:player_name]}
+
+      home_players.each do |example_player_hash|
+        expect(names).to include(example_player_hash[:player_name]), "Expected the name #{example_player_hash[:player_name]}, but could not find. Check the spelling of player names. Capitalization matters!"
+
+        player_stats = game_hash[:home][:players].find do |player|
+          player[:player_name] == example_player_hash[:player_name]
+        end
+
+        expect(player_stats.keys).to match_array(example_player_hash.keys)
+        expect(player_stats.values).to match_array(example_player_hash.values)
+      end
+    end
+  
+  
+  
+  
   
   :away => {
     :team_name => {},
