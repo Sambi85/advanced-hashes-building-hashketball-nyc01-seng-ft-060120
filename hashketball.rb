d0efb648534f1b52213ae87992c5834f7c9571eb
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-06 00:44:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,8 @@
 # Write your code here!
 require "pry"
 
-home = [alan_anderson, reggie_evans, brook_lopez, mason_plumlee, jason_terry]
-away = 
 
-def game_hash()
+def game_hash
 hash = {
   
   :home => {
